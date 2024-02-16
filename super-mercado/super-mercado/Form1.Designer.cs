b02ecde38f9900562f12f5d8816a53e1a24b223f
Pedro Henrique Alves <nukhes@protonmail.com>
2024-02-16 15:16:32 -0300

    Add files via upload

---
+++ super-mercado/super-mercado/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace super_mercado
+{
+    partial class superMercado
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNomeProduto = new System.Windows.Forms.Label();
+            this.txtNomeProduto = new System.Windows.Forms.TextBox();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.lblPreco = new System.Windows.Forms.Label();
+            this.txtDataValidade = new System.Windows.Forms.TextBox();
+            this.lblDataValidade = new System.Windows.Forms.Label();
+            this.txtCodigoDeBarras = new System.Windows.Forms.TextBox();
+            this.lblBarras = new System.Windows.Forms.Label();
+            this.txtLote = new System.Windows.Forms.TextBox();
+            this.lblLote = new System.Windows.Forms.Label();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.Quantidade = new System.Windows.Forms.Label();
+            this.txtObs = new System.Windows.Forms.TextBox();
+            this.lblObs = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblNomeProduto
+            // 
+            this.lblNomeProduto.AutoSize = true;
+            this.lblNomeProduto.Location = new System.Drawing.Point(60, 146);
+            this.lblNomeProduto.Name = "lblNomeProduto";
+            this.lblNomeProduto.Size = new System.Drawing.Size(89, 13);
+            this.lblNomeProduto.TabIndex = 0;
+            this.lblNomeProduto.Text = "Nome do produto";
+            // 
+            // txtNomeProduto
+            // 
+            this.txtNomeProduto.Location = new System.Drawing.Point(155, 143);
+            this.txtNomeProduto.Name = "txtNomeProduto";
+            this.txtNomeProduto.Size = new System.Drawing.Size(194, 20);
+            this.txtNomeProduto.TabIndex = 1;
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(155, 169);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(194, 20);
+            this.txtPreco.TabIndex = 3;
+            // 
+            // lblPreco
+            // 
+            this.lblPreco.AutoSize = true;
+            this.lblPreco.Location = new System.Drawing.Point(60, 172);
+            this.lblPreco.Name = "lblPreco";
+            this.lblPreco.Size = new System.Drawing.Size(35, 13);
+            this.lblPreco.TabIndex = 2;
+            this.lblPreco.Text = "Preço";
+            // 
+            // txtDataValidade
+            // 
+            this.txtDataValidade.Location = new System.Drawing.Point(155, 195);
+            this.txtDataValidade.Name = "txtDataValidade";
+            this.txtDataValidade.Size = new System.Drawing.Size(194, 20);
+            this.txtDataValidade.TabIndex = 5;
+            // 
+            // lblDataValidade
+            // 
+            this.lblDataValidade.AutoSize = true;
+            this.lblDataValidade.Location = new System.Drawing.Point(60, 198);
+            this.lblDataValidade.Name = "lblDataValidade";
+            this.lblDataValidade.Size = new System.Drawing.Size(74, 13);
+            this.lblDataValidade.TabIndex = 4;
+            this.lblDataValidade.Text = "Data Validade";
+            // 
+            // txtCodigoDeBarras
+            // 
+            this.txtCodigoDeBarras.Location = new System.Drawing.Point(155, 273);
+            this.txtCodigoDeBarras.Name = "txtCodigoDeBarras";
+            this.txtCodigoDeBarras.Size = new System.Drawing.Size(194, 20);
+            this.txtCodigoDeBarras.TabIndex = 11;
+            // 
+            // lblBarras
+            // 
+            this.lblBarras.AutoSize = true;
+            this.lblBarras.Location = new System.Drawing.Point(60, 276);
+            this.lblBarras.Name = "lblBarras";
+            this.lblBarras.Size = new System.Drawing.Size(87, 13);
+            this.lblBarras.TabIndex = 10;
+            this.lblBarras.Text = "Código de barras";
+            // 
+            // txtLote
+            // 
+            this.txtLote.Location = new System.Drawing.Point(155, 247);
+            this.txtLote.Name = "txtLote";
+            this.txtLote.Size = new System.Drawing.Size(194, 20);
+            this.txtLote.TabIndex = 9;
+            // 
+            // lblLote
+            // 
+            this.lblLote.AutoSize = true;
+            this.lblLote.Location = new System.Drawing.Point(60, 250);
+            this.lblLote.Name = "lblLote";
+            this.lblLote.Size = new System.Drawing.Size(28, 13);
+            this.lblLote.TabIndex = 8;
+            this.lblLote.Text = "Lote";
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(155, 221);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(194, 20);
+            this.txtQuantidade.TabIndex = 7;
+            // 
+            // Quantidade
+            // 
+            this.Quantidade.AutoSize = true;
+            this.Quantidade.Location = new System.Drawing.Point(60, 224);
+            this.Quantidade.Name = "Quantidade";
+            this.Quantidade.Size = new System.Drawing.Size(62, 13);
+            this.Quantidade.TabIndex = 6;
+            this.Quantidade.Text = "Quantidade";
+            // 
+            // txtObs
+            // 
+            this.txtObs.Location = new System.Drawing.Point(155, 299);
+            this.txtObs.Multiline = true;
+            this.txtObs.Name = "txtObs";
+            this.txtObs.Size = new System.Drawing.Size(194, 150);
+            this.txtObs.TabIndex = 13;
+            // 
+            // lblObs
+            // 
+            this.lblObs.AutoSize = true;
+            this.lblObs.Location = new System.Drawing.Point(60, 302);
+            this.lblObs.Name = "lblObs";
+            this.lblObs.Size = new System.Drawing.Size(70, 13);
+            this.lblObs.TabIndex = 12;
+            this.lblObs.Text = "Observações";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(63, 473);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(286, 45);
+            this.btnEnviar.TabIndex = 14;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.BtnEnviar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 29);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Super Mercado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.SandyBrown;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 67);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Rafael";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // superMercado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 581);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtObs);
+            this.Controls.Add(this.lblObs);
+            this.Controls.Add(this.txtCodigoDeBarras);
+            this.Controls.Add(this.lblBarras);
+            this.Controls.Add(this.txtLote);
+            this.Controls.Add(this.lblLote);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.Quantidade);
+            this.Controls.Add(this.txtDataValidade);
+            this.Controls.Add(this.lblDataValidade);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.lblPreco);
+            this.Controls.Add(this.txtNomeProduto);
+            this.Controls.Add(this.lblNomeProduto);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "superMercado";
+            this.Text = "Cadastro de Produtos";
+            this.Load += new System.EventHandler(this.SuperMercado_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNomeProduto;
+        private System.Windows.Forms.TextBox txtNomeProduto;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.Label lblPreco;
+        private System.Windows.Forms.TextBox txtDataValidade;
+        private System.Windows.Forms.Label lblDataValidade;
+        private System.Windows.Forms.TextBox txtCodigoDeBarras;
+        private System.Windows.Forms.Label lblBarras;
+        private System.Windows.Forms.TextBox txtLote;
+        private System.Windows.Forms.Label lblLote;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label Quantidade;
+        private System.Windows.Forms.TextBox txtObs;
+        private System.Windows.Forms.Label lblObs;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ super-mercado/super-mercado/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace super_mercado
+{
+    public partial class superMercado : Form
+    {
+        public superMercado()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnEnviar_Click(object sender, EventArgs e)
+        {
+            string nome, observacoes;
+            decimal preco;
+            int quantidade, lote, codigoBarras;
+
+            nome = txtNomeProduto.ToString();
+            observacoes = txtObs.ToString();
+            preco = Convert.ToDecimal(txtPreco.Text);
+            quantidade = Convert.ToInt32(txtQuantidade.Text);
+            lote = Convert.ToInt32(txtLote.Text);
+            codigoBarras = Convert.ToInt32(txtCodigoDeBarras.Text);
+
+            MessageBox.Show("Cadastrado com Sucesso!");
+        }
+
+        private void SuperMercado_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
